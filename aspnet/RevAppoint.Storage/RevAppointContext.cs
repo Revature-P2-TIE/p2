2fbad151d193ec536e3b585f29648a104cd42f9f
isaiah <isotodev@gmail.com>
2021-01-21 00:45:51 -0500

    Added DbContext

---
+++ aspnet/RevAppoint.Storage/RevAppointContext.cs
@@ -0,0 +1,28 @@
+namespace RevAppoint.Storage
+{
+    public class RevAppointContext : DbContext
+    {
+        public DbSet<Appointment> Appointment {get;set;}
+        public DbSet<Professional> Professional {get;set;}
+        public DbSet<Time> Time {get;set;}
+        public DbSet<User> User {get;set;}
+
+        public RevAppointContext(DbContextOptions<RevAppointContext> options) : base(options){}
+        public RevAppointContext(){}
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Appointment>.HasKey(s => s.EntityID);
+            builder.Entity<Professional>.HasKey(s => s.EntityID);
+            builder.Entity<User>.HasKey(s => s.EntityID);
+            builder.Entity<Time>.HasKey(s => s.EntityID);
+
+            builder.Entity<Appointment>.Property(p => p.EntityID).ValueGeneratedNever();
+            builder.Entity<Professional>.Property(p => p.EntityID).ValueGeneratedNever();
+            builder.Entity<User>.Property(p => p.EntityID).ValueGeneratedNever();
+            builder.Entity<Time>.Property(p => p.EntityID).ValueGeneratedNever();
+
+            SeedDate(builder);
+        }
+    }
+}
