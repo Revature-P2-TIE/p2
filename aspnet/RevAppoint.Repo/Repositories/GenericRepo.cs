f0d225848167456e3428eeeec5f5b30705d6a92c
isaiah <isotodev@gmail.com>
2021-01-21 01:33:24 -0500

    Added a Generic Repo

---
+++ aspnet/RevAppoint.Repo/Repositories/GenericRepo.cs
@@ -0,0 +1,49 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using RevAppoint.Storage;
+
+namespace RevAppoint.Repo.Repositories
+{
+    public class GenericRepo<T> where T : class
+    {
+        private RevAppointContext _context;
+        private DbSet<T> table;
+        public GenericRepo()
+        {
+            this._context = new RevAppointContext();
+            table = _context.Set<T>();
+        }
+        public GenericRepo(RevAppointContext _context)
+        {
+            this._context = _context;
+            table = _context.Set<T>();
+        }
+        public IEnumerable<T> GetAll()
+        {
+            return table.ToList();
+        }
+        public T GetById(object id)
+        {
+            return table.Find(id);
+        }
+        public void Insert(T obj)
+        {
+            table.Add(obj);
+        }
+        public void Update(T obj)
+        {
+            table.Attach(obj);
+            _context.Entry(obj).State = EntityState.Modified;
+        }
+        public void Delete(object id)
+        {
+            T existing = table.Find(id);
+            table.Remove(existing);
+        }
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
