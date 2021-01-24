8fae2b2b9db3d5a70f2cdec17a1737610cfd3b49
isaiah <isotodev@gmail.com>
2021-01-23 21:34:02 -0500

    Cant Remember

--- aspnet/RevAppoint.Client/Models/AppointmentViewModel.cs
+++ aspnet/RevAppoint.Client/Models/AppointmentViewModel.cs
@@ -1,9 +1,12 @@
-
+using RevAppoint.Domain.POCOs;
 
 namespace RevAppoint.Client.Models
 {
     public class AppointmentViewModel
     {
+        Customer Customer {get;set;}
+        Professional Professional{get;set;}
+        Appointment Appointment{get;set;}
 
     }
 }
