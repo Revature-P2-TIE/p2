8426cdb30e8e7552a7b10f313e1b1ff4027833dd
elmer tsai <chenyu.tsai@revature.net>
2021-01-25 19:33:03 -0500

    updated migration

--- aspnet/RevAppoint.Storage/Migrations/20210126002909_More properties for User and Professional Added.cs
+++ aspnet/RevAppoint.Storage/Migrations/20210126002909_More properties for User and Professional Added.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace RevAppoint.Storage.Migrations
 {
-    public partial class AddedIsAccepted : Migration
+    public partial class MorepropertiesforUserandProfessionalAdded : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
@@ -28,7 +28,12 @@ namespace RevAppoint.Storage.Migrations
                     Username = table.Column<string>(type: "nvarchar(max)", nullable: true),
                     Password = table.Column<string>(type: "nvarchar(max)", nullable: true),
                     FirstName = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Gender = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    EmailAddress = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    MemberSince = table.Column<DateTime>(type: "datetime2", nullable: false)
                 },
                 constraints: table =>
                 {
@@ -60,7 +65,11 @@ namespace RevAppoint.Storage.Migrations
                     Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
                     Location = table.Column<string>(type: "nvarchar(max)", nullable: true),
                     AvailableTimeEntityID = table.Column<long>(type: "bigint", nullable: true),
-                    AppointmentLengthInHours = table.Column<int>(type: "int", nullable: false)
+                    AppointmentLengthInHours = table.Column<int>(type: "int", nullable: false),
+                    HourlyRate = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Language = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Bio = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Rating = table.Column<double>(type: "float", nullable: false)
                 },
                 constraints: table =>
                 {
@@ -115,23 +124,23 @@ namespace RevAppoint.Storage.Migrations
 
             migrationBuilder.InsertData(
                 table: "User",
-                columns: new[] { "EntityID", "FirstName", "LastName", "Password", "Username" },
+                columns: new[] { "EntityID", "EmailAddress", "FirstName", "Gender", "LastName", "MemberSince", "Password", "PhoneNumber", "Type", "Username" },
                 values: new object[,]
                 {
-                    { 637470557392729784L, "Melvin", "Mac", "Password", "Username1" },
-                    { 637470557392765176L, "Barbara", "Gross", "Password", "trombone" },
-                    { 637470557392765257L, "Faiza", "Bowman", "Password", "chicken" },
-                    { 637470557392765264L, "Nathalie", "Fellows", "Password", "foxtrail" },
-                    { 637470557392765268L, "Barney", "Simons", "Password", "perseus" },
-                    { 637470557392765284L, "Adrianna", "Prentice", "Password", "applepie" },
-                    { 637470557392765289L, "Maxim", "Fowler", "Password", "candyfog" },
-                    { 637470557392789599L, "Shelley", "Stacey", "BadPassword", "banjojudo" },
-                    { 637470557392792364L, "Salgado", "Arnie", "BadPassword", "hotdogpeas" },
-                    { 637470557392792407L, "Chanel", "Tamera", "BadPassword", "psychobatman" },
-                    { 637470557392792414L, "Lawrence", "Gregg", "BadPassword", "brownbread" },
-                    { 637470557392792419L, "Ibrahim", "Elis", "BadPassword", "oystersnatch" },
-                    { 637470557392792442L, "Page", "Branch", "BadPassword", "islandhorse" },
-                    { 637470557392792450L, "Chante", "Jacob", "BadPassword", "cocktailwave" }
+                    { 637471997487521337L, "yeye@gmail.com", "Melvin", "F", "Mac", new DateTime(2021, 1, 25, 19, 29, 8, 753, DateTimeKind.Local).AddTicks(3650), "Password", "(773)-555-1234", "Customer", "Username1" },
+                    { 637471997487536087L, "rockout@gmail.com", "Barbara", "M", "Gross", new DateTime(2021, 1, 25, 19, 29, 8, 753, DateTimeKind.Local).AddTicks(6094), "Password", "(773)-555-1233", "Customer", "trombone" },
+                    { 637471997487536099L, "playwithme@gmail.com", "Faiza", "F", "Bowman", new DateTime(2021, 1, 25, 19, 29, 8, 753, DateTimeKind.Local).AddTicks(6101), "Password", "(773)-555-1241", "Customer", "chicken" },
+                    { 637471997487536103L, "REviw@gmail.com", "Nathalie", "M", "Fellows", new DateTime(2021, 1, 25, 19, 29, 8, 753, DateTimeKind.Local).AddTicks(6104), "Password", "(773)-555-1234", "Customer", "foxtrail" },
+                    { 637471997487536106L, "HotBatman@gmail.com", "Barney", "F", "Simons", new DateTime(2021, 1, 25, 19, 29, 8, 753, DateTimeKind.Local).AddTicks(6107), "Password", "(773)-555-1144", "Customer", "perseus" },
+                    { 637471997487536112L, "Tootoo@gmail.com", "Adrianna", "M", "Prentice", new DateTime(2021, 1, 25, 19, 29, 8, 753, DateTimeKind.Local).AddTicks(6113), "Password", "(773)-555-1234", "Customer", "applepie" },
+                    { 637471997487536208L, "harhar@gmail.com", "Maxim", "M", "Fowler", new DateTime(2021, 1, 25, 19, 29, 8, 753, DateTimeKind.Local).AddTicks(6210), "Password", "(773)-555-1234", "Customer", "candyfog" },
+                    { 637471997487544649L, null, "Shelley", null, "Stacey", new DateTime(2021, 1, 25, 19, 29, 8, 754, DateTimeKind.Local).AddTicks(4668), "BadPassword", null, "Professional", "banjojudo" },
+                    { 637471997487546303L, null, "Salgado", null, "Arnie", new DateTime(2021, 1, 25, 19, 29, 8, 754, DateTimeKind.Local).AddTicks(6316), "BadPassword", null, "Professional", "hotdogpeas" },
+                    { 637471997487546320L, null, "Chanel", null, "Tamera", new DateTime(2021, 1, 25, 19, 29, 8, 754, DateTimeKind.Local).AddTicks(6323), "BadPassword", null, "Professional", "psychobatman" },
+                    { 637471997487546326L, null, "Lawrence", null, "Gregg", new DateTime(2021, 1, 25, 19, 29, 8, 754, DateTimeKind.Local).AddTicks(6328), "BadPassword", null, "Professional", "brownbread" },
+                    { 637471997487546331L, null, "Ibrahim", null, "Elis", new DateTime(2021, 1, 25, 19, 29, 8, 754, DateTimeKind.Local).AddTicks(6333), "BadPassword", null, "Professional", "oystersnatch" },
+                    { 637471997487546340L, null, "Page", null, "Branch", new DateTime(2021, 1, 25, 19, 29, 8, 754, DateTimeKind.Local).AddTicks(6342), "BadPassword", null, "Professional", "islandhorse" },
+                    { 637471997487546345L, null, "Chante", null, "Jacob", new DateTime(2021, 1, 25, 19, 29, 8, 754, DateTimeKind.Local).AddTicks(6347), "BadPassword", null, "Professional", "cocktailwave" }
                 });
 
             migrationBuilder.InsertData(
@@ -139,27 +148,27 @@ namespace RevAppoint.Storage.Migrations
                 column: "EntityID",
                 values: new object[]
                 {
-                    637470557392729784L,
-                    637470557392765176L,
-                    637470557392765257L,
-                    637470557392765264L,
-                    637470557392765268L,
-                    637470557392765284L,
-                    637470557392765289L
+                    637471997487521337L,
+                    637471997487536087L,
+                    637471997487536099L,
+                    637471997487536103L,
+                    637471997487536106L,
+                    637471997487536112L,
+                    637471997487536208L
                 });
 
             migrationBuilder.InsertData(
                 table: "Professional",
-                columns: new[] { "EntityID", "AppointmentLengthInHours", "AvailableTimeEntityID", "Location", "Title" },
+                columns: new[] { "EntityID", "AppointmentLengthInHours", "AvailableTimeEntityID", "Bio", "HourlyRate", "Language", "Location", "Rating", "Title" },
                 values: new object[,]
                 {
-                    { 637470557392789599L, 1, null, "Chicago", "Blacksmith" },
-                    { 637470557392792364L, 2, null, "Las Vegas", "Web Developer" },
-                    { 637470557392792407L, 3, null, "Las Vegas", "Driver" },
-                    { 637470557392792414L, 1, null, "New York", "Nurse" },
-                    { 637470557392792419L, 2, null, "New York", "Barber" },
-                    { 637470557392792442L, 4, null, "Chicago", "Barber" },
-                    { 637470557392792450L, 12, null, "Chicago", "Web Developer" }
+                    { 637471997487544649L, 1, null, null, 0m, null, "Chicago", 5.0, "Blacksmith" },
+                    { 637471997487546303L, 2, null, null, 0m, null, "Las Vegas", 5.0, "Web Developer" },
+                    { 637471997487546320L, 3, null, null, 0m, null, "Las Vegas", 5.0, "Driver" },
+                    { 637471997487546326L, 1, null, null, 0m, null, "New York", 5.0, "Nurse" },
+                    { 637471997487546331L, 2, null, null, 0m, null, "New York", 5.0, "Barber" },
+                    { 637471997487546340L, 4, null, null, 0m, null, "Chicago", 5.0, "Barber" },
+                    { 637471997487546345L, 12, null, null, 0m, null, "Chicago", 5.0, "Web Developer" }
                 });
 
             migrationBuilder.CreateIndex(
