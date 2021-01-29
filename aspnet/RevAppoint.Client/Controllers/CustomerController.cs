c4d33e9aa67125e41e783d99b72d932fe72afa31
isaiah <isotodev@gmail.com>
2021-01-28 20:20:36 -0500

    DI unitofwork

---
+++ aspnet/RevAppoint.Client/Controllers/CustomerController.cs
@@ -0,0 +1,17 @@
+  using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace RevAppoint.Client.Controllers.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CustomerController:ControllerBase
+    {
+        
+    }
+}
--- aspnet/RevAppoint.Client/Startup.cs
+++ aspnet/RevAppoint.Client/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using RevAppoint.Repo.Repositories;
 
 namespace RevAppoint.Client
 {
@@ -32,6 +33,7 @@ namespace RevAppoint.Client
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "RevAppoint.Client", Version = "v1" });
             });
+            services.AddScoped<UnitOfWork>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
