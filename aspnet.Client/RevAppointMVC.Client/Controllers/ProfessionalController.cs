b86e29621e084d34079e0e3e1dba5fedda1c652b
Nicholas Hall <nthall1914@gmail.com>
2021-01-30 00:23:53 -0500

    added ProAccountInfo back in and lost code in proController

--- aspnet.Client/RevAppointMVC.Client/Controllers/ProfessionalController.cs
+++ aspnet.Client/RevAppointMVC.Client/Controllers/ProfessionalController.cs
@@ -93,4 +93,57 @@ namespace RevAppoint.Client.Controllers
             return View("ClientView", model);
         }
 
+        [Route("[action]")]
+        [HttpPost]
+        public IActionResult CompleteAppointment(ProfessionalViewModel model)
+        {
+
+            Appointment appointment = Repo.GetById<Appointment>(long.Parse(model.AppointmentID));
+            appointment.IsFufilled = true;
+            Repo.Save();
+            model.Professional = Repo.ProfessionalRepo.GetProfessional(model.Username);
+            // model.Username = model.Professional.Username;
+
+            model.CurrentAppointmets = Repo.ProfessionalRepo.GetAppointments(model.Professional.EntityID).ToList();
+            return View("CurrentAppointments", model);
+        }
+        [Route("[action]")]
+        [HttpPost]
+        public IActionResult AcceptAppointment(ProfessionalViewModel model)
+        {
+            Appointment appointment = Repo.GetById<Appointment>(long.Parse(model.AppointmentID));;
+            appointment.IsAccepted = !appointment.IsAccepted;
+            Repo.Save();
+            model.Professional = Repo.ProfessionalRepo.GetProfessional(model.Username);
+            model.CurrentAppointmets = Repo.ProfessionalRepo.GetAppointments(model.Professional.EntityID).ToList();
+            return View("CurrentAppointments", model);
+        }
+
+        [HttpGet("~/Professional/ProfessionalAccountView/{id}")]
+        public IActionResult ProfessionalAccountView(string id)
+        {
+            ProfessionalViewModel model = new ProfessionalViewModel();
+            model.Professional = Repo.ProfessionalRepo.GetProfessional(id);
+            model.Username = model.Professional.Username;
+
+            return View("ProfessionalAccountView", model);
+        }
+
+        [HttpPost("~/Professional/ProfessionalAccountView/{id}")]
+        public IActionResult EditAccountInfo(string id, ProfessionalViewModel model)
+        {            
+            ProfessionalViewModel newModel = new ProfessionalViewModel();
+            newModel.Professional = Repo.ProfessionalRepo.GetProfessional(id);
+            newModel.Professional.Title = model.Professional.Title;
+            newModel.Professional.Location = model.Professional.Location;
+            newModel.Professional.AppointmentLengthInHours = model.Professional.AppointmentLengthInHours;
+            newModel.Professional.HourlyRate = model.Professional.HourlyRate;
+            newModel.Professional.Language = model.Professional.Language;
+            newModel.Professional.Bio = model.Professional.Bio;
+            newModel.Username = id;
+            Repo.Update(newModel.Professional);
+            Repo.Save();
+            return View("ProfessionalAccountView", newModel);
+        }
+
         */
