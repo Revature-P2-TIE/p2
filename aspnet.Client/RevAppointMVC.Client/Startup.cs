a11626e6fb3b7a104e323f571cfe185d2fc1253a
isaiah <isotodev@gmail.com>
2021-02-03 23:49:05 -0500

    changed error page

--- aspnet.Client/RevAppointMVC.Client/Startup.cs
+++ aspnet.Client/RevAppointMVC.Client/Startup.cs
@@ -53,7 +53,8 @@ namespace RevAppoint.Client
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                // app.UseExceptionHandler("/Home/Error");
+                app.UseDeveloperExceptionPage();
                 // // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 // app.UseHsts();
             }
